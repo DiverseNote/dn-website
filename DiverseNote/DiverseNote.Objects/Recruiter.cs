abb1f282aa8bca84e7ba70db14390062c35f1db3
LeQuin Lee <quinnlee@outlook.com>
2016-02-28 17:08:25 -0500

    updated npm packages. Bootstrap was missing.

--- DiverseNote/DiverseNote.Objects/Recruiter.cs
+++ DiverseNote/DiverseNote.Objects/Recruiter.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using DiverseNote.Objects.SearchCriteria;
-
-namespace DiverseNote.Objects
+﻿namespace DiverseNote.Objects
 {
     public class Recruiter
     {
